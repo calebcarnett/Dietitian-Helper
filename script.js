7116e20faf9dad4b02942e37cfbc71356c733aa8
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-06 15:08:28 -0500

    modified layout

--- script.js
+++ script.js
@@ -12,62 +12,62 @@ function BMI(bmi) {
 
     bmi = Math.round(bmi * 10) / 10;
     bmiResult.textContent = "BMI: " + bmi;
-    return console.log(bmi);
+    return bmi;
   } else {
     alert("For BMI you must enter both weight and height");
   }
 }
 
-function mifflinMale() {
-  let weight = document.getElementById("weightInput").value.trim();
-  let height = document.getElementById("heightInput").value.trim();
-  let age = document.getElementById("ageInput").value.trim();
-  //convert height to centimeters
-  height = height * 2.54;
-  //convert pounds to kilograms
-  weight = weight * 0.45359237;
-  if (input === male) {
-    mifflinM = weight * 10 + 6.25 * height - 5 * age + 5;
-  }
-}
-
-function mifflinFemale() {
-  let weight = document.getElementById("weightInput").value.trim();
-  let height = document.getElementById("heightInput").value.trim();
-  let age = document.getElementById("ageInput").value.trim();
-  //convert height to centimeters
-  height = height * 2.54;
-  //convert pounds to kilograms
-  weight = weight * 0.45359237;
-  if (input === female) {
-    mifflinF = weight * 10 + 6.25 * height - 5 * age - 161;
-  }
-}
-
-function harrisBenedictMale() {
-  let weight = document.getElementById("weightInput").value.trim();
-  let height = document.getElementById("heightInput").value.trim();
-  let age = document.getElementById("ageInput").value.trim();
-  //convert height to centimeters
-  height = height * 2.54;
-  //convert pounds to kilograms
-  weight = weight * 0.45359237;
-  if (input === male) {
-    harrisMale = 66 + 13.8 * weight + 5 * height - 6.8 * age;
-  }
-}
-
-function harrisBenedictFemale() {
-  let weight = document.getElementById("weightInput").value.trim();
-  let height = document.getElementById("heightInput").value.trim();
-  let age = document.getElementById("ageInput").value.trim();
-  //convert height to centimeters
-  height = height * 2.54;
-  //convert pounds to kilograms
-  weight = weight * 0.45359237;
-  if (input === male) {
-    harrisFemale = 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
-  }
-}
-
 button.addEventListener("click", BMI);
+
+// function mifflinMale() {
+//   let weight = document.getElementById("weightInput").value.trim();
+//   let height = document.getElementById("heightInput").value.trim();
+//   let age = document.getElementById("ageInput").value.trim();
+//   //convert height to centimeters
+//   height = height * 2.54;
+//   //convert pounds to kilograms
+//   weight = weight * 0.45359237;
+//   if (input === male) {
+//     mifflinM = weight * 10 + 6.25 * height - 5 * age + 5;
+//   }
+// }
+
+// function mifflinFemale() {
+//   let weight = document.getElementById("weightInput").value.trim();
+//   let height = document.getElementById("heightInput").value.trim();
+//   let age = document.getElementById("ageInput").value.trim();
+//   //convert height to centimeters
+//   height = height * 2.54;
+//   //convert pounds to kilograms
+//   weight = weight * 0.45359237;
+//   if (input === female) {
+//     mifflinF = weight * 10 + 6.25 * height - 5 * age - 161;
+//   }
+// }
+
+// function harrisBenedictMale() {
+//   let weight = document.getElementById("weightInput").value.trim();
+//   let height = document.getElementById("heightInput").value.trim();
+//   let age = document.getElementById("ageInput").value.trim();
+//   //convert height to centimeters
+//   height = height * 2.54;
+//   //convert pounds to kilograms
+//   weight = weight * 0.45359237;
+//   if (input === male) {
+//     harrisMale = 66 + 13.8 * weight + 5 * height - 6.8 * age;
+//   }
+// }
+
+// function harrisBenedictFemale() {
+//   let weight = document.getElementById("weightInput").value.trim();
+//   let height = document.getElementById("heightInput").value.trim();
+//   let age = document.getElementById("ageInput").value.trim();
+//   //convert height to centimeters
+//   height = height * 2.54;
+//   //convert pounds to kilograms
+//   weight = weight * 0.45359237;
+//   if (input === male) {
+//     harrisFemale = 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
+//   }
+// }
